8cdc7091c7b4a218907e83017cd865ca8d4bd072
mkpoli <git@mkpo.li>
2024-10-18 14:46:54 +0900

    refactor: extract Editor as a component

---
+++ src/lib/Editor.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  const segmenter = new Intl.Segmenter('en', { granularity: 'word' });
+  let { text = $bindable() }: { text: string } = $props();
+</script>
+
+<textarea class="h-max resize-vertical min-h-16" bind:value={text}></textarea>
+<output class="h-full">
+  {#each segmenter.segment(text) as segment}
+    <span
+      class="hover:bg-green-200/50"
+      title={[...segment.segment]
+        .map((char) => char.codePointAt(0))
+        .map((code) => (code ? `U+${code.toString(16).toUpperCase()}` : ''))
+        .join(' ')}
+    >
+      {segment.segment}
+    </span>
+  {/each}
+</output>
+
+<style lang="postcss">
+  textarea,
+  output {
+    font-family: 'Jigmo', sans-serif;
+    @apply border border-black p-2 text-xl;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,11 @@
 <script lang="ts">
-  const segmenter = new Intl.Segmenter('en', { granularity: 'word' });
+  import Editor from '$lib/Editor.svelte';
+
   let text = $state('精󠄀');
 </script>
 
 <h1 class="text-center w-full text-2xl my-4">Variant Selector Dictionary</h1>
 
 <main class="max-w-screen-md mx-auto grid grid-cols-2 gap-4">
-  <textarea class="h-max resize-vertical min-h-16" bind:value={text}></textarea>
-  <output class="h-full">
-    {#each segmenter.segment(text) as segment}
-      <span
-        class="hover:bg-green-200/50"
-        title={[...segment.segment]
-          .map((char) => char.codePointAt(0))
-          .map((code) => (code ? `U+${code.toString(16).toUpperCase()}` : ''))
-          .join(' ')}
-      >
-        {segment.segment}
-      </span>
-    {/each}
-  </output>
+  <Editor {text} />
 </main>
-
-<style lang="postcss">
-  textarea,
-  output {
-    font-family: 'Jigmo', sans-serif;
-    @apply border border-black p-2 text-xl;
-  }
-</style>

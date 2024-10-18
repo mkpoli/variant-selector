9d2ef4f4a3ff6e79f39f99c00a890758121e32eb
mkpoli <git@mkpo.li>
2024-10-18 15:06:11 +0900

    refactor; use tailwind to manage Jigmo font

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -12,14 +12,14 @@
 </script>
 
 <textarea
-  class="h-max resize-vertical min-h-16"
+  class="h-max resize-vertical min-h-16 font-jigmo"
   bind:value={text}
   onselectionchange={(e) => (selection = { start: e.currentTarget.selectionStart, end: e.currentTarget.selectionEnd })}
 ></textarea>
 <output class="h-full">
   {#each segment(text) as seg}
     <span
-      class="hover:bg-green-200/50"
+      class="hover:bg-green-200/50 font-jigmo"
       title={[...seg]
         .map((char) => char.codePointAt(0))
         .map((code) => (code ? `U+${code.toString(16).toUpperCase()}` : ''))
@@ -33,7 +33,6 @@
 <style lang="postcss">
   textarea,
   output {
-    font-family: 'Jigmo', sans-serif;
     @apply border border-black p-2 text-xl;
   }
 </style>

2823079ee8c5b757f5e00a0a6253c3bb14355854
mkpoli <git@mkpo.li>
2024-10-18 20:26:24 +0900

    feat: replace output by vertical

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import { segment } from '$lib/text';
+  import { codePoints, formatCodePoint, segment } from '$lib/text';
 
   import type { Editor } from '$lib/editor.svelte';
+  import { isCJKUnifiedIdeograph, isVariationSelector } from './unicode';
 
   let { editor }: { editor: Editor } = $props();
 
@@ -13,9 +14,28 @@
   });
 </script>
 
-<div class="grid grid-cols-2 gap-4">
-  <textarea bind:this={textarea} class="h-max resize-vertical min-h-16 font-jigmo"></textarea>
-  <output class="h-full">
+<div class="grid grid-cols-[1fr_auto] gap-4">
+  <textarea bind:this={textarea} class="h-max min-h-full font-jigmo"></textarea>
+  <output class="h-full min-w-48">
+    <div class="grid grid-cols-[auto_1fr] gap-x-4 gap-y-1 mx-auto">
+      {#each segment(editor.selectedText || editor.text) as char}
+        <span class="font-jigmo">{char}</span>
+        <code class="flex flex-wrap gap-1">
+          {#each codePoints(char) as code}
+            <span
+              class="px-1"
+              class:unified-ideograph={isCJKUnifiedIdeograph(code)}
+              class:variation-selector={isVariationSelector(code)}
+              title={String.fromCodePoint(code)}
+            >
+              {formatCodePoint(code)}
+            </span>
+          {/each}
+        </code>
+      {/each}
+    </div>
+  </output>
+  <!-- <output class="h-full">
     {#each segment(editor.text) as seg}
       <span
         class="hover:bg-green-200/50 font-jigmo"
@@ -27,12 +47,18 @@
         {seg}
       </span>
     {/each}
-  </output>
+  </output> -->
 </div>
 
 <style lang="postcss">
-  textarea,
-  output {
+  textarea {
     @apply border border-black p-2 text-xl;
+  }
+
+  .unified-ideograph {
+    @apply bg-blue-200;
+  }
+  .variation-selector {
+    @apply bg-green-200;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
   import Editor from '$lib/Editor.svelte';
-  import { segment, codePoints, formatCodePoint } from '$lib/text';
   import { createEditor } from '$lib/editor.svelte';
   import VariableSelectorInputButton from '$lib/VariantSelectorInputButton.svelte';
   import { dev } from '$app/environment';
-  import { isCJKUnifiedIdeograph, isVariationSelector } from '$lib/unicode';
   let editor = createEditor(dev ? '精󠄀清從心邪' : '');
 </script>
 
 <h1 class="text-center w-full text-2xl my-4">Variant Selector Dictionary</h1>
 
-<main class="max-w-screen-md mx-auto flex flex-col gap-4">
+<main class="max-w-screen-sm mx-auto flex flex-col gap-4">
   <Editor {editor} />
   <div>
     <VariableSelectorInputButton
@@ -19,30 +17,4 @@
       }}
     />
   </div>
-  <div class="grid grid-cols-[auto_1fr] gap-x-4 gap-y-1 mx-auto">
-    {#each segment(editor.selectedText || editor.text) as char}
-      <span class="font-jigmo">{char}</span>
-      <code class="flex flex-wrap gap-1">
-        {#each codePoints(char) as code}
-          <span
-            class="px-1"
-            class:unified-ideograph={isCJKUnifiedIdeograph(code)}
-            class:variation-selector={isVariationSelector(code)}
-            title={String.fromCodePoint(code)}
-          >
-            {formatCodePoint(code)}
-          </span>
-        {/each}
-      </code>
-    {/each}
-  </div>
 </main>
-
-<style lang="postcss">
-  .unified-ideograph {
-    @apply bg-blue-200;
-  }
-  .variation-selector {
-    @apply bg-green-200;
-  }
-</style>
